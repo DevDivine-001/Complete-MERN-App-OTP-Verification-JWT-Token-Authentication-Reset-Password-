c6493f757d7c1c84b928f8cdf28d181a9e9d1b1e
Oscdivin <divineuwandu84@Gmail.com>
2024-08-02 03:43:52 +0100

    i'm gone on Username

--- src/components/Username.jsx
+++ src/components/Username.jsx
@@ -4,22 +4,22 @@ import Styles from "../Styles/Username.module.css"
 export default function Username() {
   return (
     <div className="container mx-auto ">
-      <div className="flex h-screen">
+      <div className="flex h-screen justify-center items-center">
         <div className={Styles.glass}>
           <div className="title flex flex-col items-center">
-            <h4 className="text-5xl font-bold">Hello Divine</h4>
-            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+            <h4 className="text-5xl font-bold max-sm:text-3xl">Hello Divine</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500 max-sm:text-sm">
               Explore More by connecting With us.
 
             </span>
           </div>
-          <form action="" className="py-1">
+          <form action="" className="py-1 gap-3 bg-[] flex flex-col">
             <div className="profile flex justify-center py-4">
               <img src={avatar} className={Styles.profile_img} alt="avatar" />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input type="text" placeholder="Username"  className={Styles.textbox}/>
+              <input type="text" placeholder="Username" className={Styles.textbox} />
               <button type="submit" className={Styles.btn}>Let Go</button>
 
             </div>
