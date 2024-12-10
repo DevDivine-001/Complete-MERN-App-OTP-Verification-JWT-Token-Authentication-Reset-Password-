f3a684cb65b56813f5c43b9842eeb3b5417b0f20
Oscdivin <divineuwandu84@Gmail.com>
2024-12-10 11:30:10 +0100

    mode is update

--- auth-api/index.js
+++ auth-api/index.js
@@ -10,7 +10,7 @@ dotenv.config()
 
 
 const app = express()
-
+multer(multer())
 
 
 const port = 8080
--- auth-api/model/UserModel.js
+++ auth-api/model/UserModel.js
@@ -96,51 +96,51 @@
 // export default mongoose.model('User', UserSchema);
 
 
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// // Username
-// // F Name
-// // L Name
-// // Password
+// Username
+// F Name
+// L Name
+// Password
 
-// export const userSchema = new mongoose.Schema({
-//     UserName: {
-//         type: String,
-//         require: true,
-//         unique: true,
-//     },
-//     FirstName: {
-//         type: String,
-//         require: true,
-//         unique: true,
-//     },
-//     LastName: {
-//         type: String,
-//         require: true,
-//         unique: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//         lowercase: true,
+export const userSchema = new mongoose.Schema({
+    UserName: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    FirstName: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    LastName: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
 
-//         match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
-//         validate: {
-//             validator: function (value) {
-//                 const fakeDomains = ["tempmail.com", "10minutemail.com", "mailinator.com"];
-//                 const domain = value.split('@')[1];
-//                 return !fakeDomains.includes(domain);
-//             },
-//             message: "Please use a real email address, not a disposable one."
-//         }
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+        validate: {
+            validator: function (value) {
+                const fakeDomains = ["tempmail.com", "10minutemail.com", "mailinator.com"];
+                const domain = value.split('@')[1];
+                return !fakeDomains.includes(domain);
+            },
+            message: "Please use a real email address, not a disposable one."
+        }
 
-//     },
-//     Password: {
-//         type: String,
-//         require: true
-//     }
+    },
+    Password: {
+        type: String,
+        require: true
+    }
 
-// }, { timestamps: true })
+}, { timestamps: true })
 
-// export const User = mongoose.model('user', userSchema)
+export const User = mongoose.model('user', userSchema)
